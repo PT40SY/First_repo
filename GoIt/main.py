9b5031c18162def7636c6e8c16ad9007a7f11de5
PT40SY <doma27@ukr.net>
2025-02-13 22:54:59 +0200

    додано обробка винятків

--- GoIt/main.py
+++ GoIt/main.py
@@ -1,9 +1,13 @@
 from datetime import datetime
 
 def get_days_from_today(date):
-    given_date = datetime.strptime(date, "%Y-%m-%d").date()
-    today_date = datetime.today().date()
-    delta = today_date - given_date
-    return delta.days
+    try:
+        given_date = datetime.strptime(date, "%Y-%m-%d").date()
+        today_date = datetime.today().date()
+        delta = today_date - given_date
+        return delta.days
+    except ValueError:
+        return "Некоректний формат дати. Будь ласка, використовуйте формат YYYY-MM-DD."
 
 print(get_days_from_today("1990-01-28"))
+print(get_days_from_today("некоректна дата"))
