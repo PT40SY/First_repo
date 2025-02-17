f1196b9016864d6afb20de80379b648c286a191a
PT40SY <doma27@ukr.net>
2025-02-17 12:09:26 +0200

    Modul_4_DZ_2

---
+++ GoIt/Modul_4_DZ_2.py
@@ -0,0 +1,23 @@
+def get_cats_info(path):
+    cats = []
+    try:
+        with open(path, 'r', encoding='utf-8') as file:
+            for line in file:
+                cat_id, name, age = line.strip().split(',')
+                cat_info = {
+                    "id": cat_id,
+                    "name": name,
+                    "age": age
+                }
+                cats.append(cat_info)
+        return cats
+    except FileNotFoundError:
+        print("Файл не знайдено.")
+        return []
+    except Exception as e:
+        print(f"Сталася помилка: {e}")
+        return []
+
+path = "path/to/cats_file.txt"
+cats_info = get_cats_info(path)
+print(cats_info)
